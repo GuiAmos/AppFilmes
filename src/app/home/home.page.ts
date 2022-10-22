7fbdc6c763d6ff73a796c4399c3ca8f05e1f92ab
GuiAmos <guilherme_amos@hotmail.com>
2022-10-21 22:20:18 -0300

    Inicio instalação API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FilmesService } from '../service/filmes.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,25 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private filmesService: FilmesService) {}
+
+  async ngOnInit(){
+    console.log('passou no home');
+    this.carregarFilmes();
+    //this.filmesService.getProductions();
+  }
+  async carregarFilmes(){
+    this.filmes = await this.filmesService.getProductions();
+    console.log("filmes carregados", this.filmes)
+    const [firstkey] = Object.keys(this.filmes);
+    this.destaque = this.filmes[firstkey];
+
+    this.filmesService.splice(firstkey, 1)
+    console.log('firstkey', firstkey)
+    console.log('destque',this.destaque)
+    console.log('filmes', this.filmes)
+  }
 
   pageContato(){
     this.router.navigate(['/detalhes'])
